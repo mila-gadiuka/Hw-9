0ea26a0c3ce4501f02592df5f18a4188dce4389c
milena <miladar050813@gmail.com>
2023-03-31 11:14:54 +0300

    база для кода

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+                task1();
+                task2();
+                task3();
+                task4();
+            }
+            public static void task1 (){
+
+            }
+            public static void task2 (){
+
+            }
+            public static void task3 (){
+
+            }
+            public static void task4 (){
+
+            }
+}
